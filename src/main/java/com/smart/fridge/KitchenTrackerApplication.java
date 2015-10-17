0ea6446cab300ebec8cc4d949bba344398664fe5
He1md4ll <martin.suckau@web.de>
2015-10-17 12:39:52 +0200

    Initial commit

---
+++ src/main/java/com/smart/fridge/KitchenTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.smart.fridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenTrackerApplication.class, args);
+    }
+}
